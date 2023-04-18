8ba4ba8bab8163d1a122e223507095d0b3e31179
AntoniaGranit <antonia.granit@gmail.com>
2023-04-18 11:39:19 +0300

    fix cart item

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -2,20 +2,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const cart = createSlice({
-    name: 'cart',
-    initialState: {
-      items: []
-    },
-    reducers: {
-      addItem: (state, action) => {
-        const existingProduct = state.items.find((item) => item.id === action.payload.id)
+  name: 'cart',
+  initialState: {
+    items: []
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const existingProduct = state.items.find((item) => item.id === action.payload.id)
 
-        if (existingProduct) {
+      if (existingProduct) {
         existingProduct.quantity += 1;
-      }
-      else {
+      } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
+    },
+
+    removeItem: (state, action) => {
+      const existingProduct = state.items.find((item) => item.id === action.payload.id)
+
+      if (existingProduct && existingProduct.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== action.payload.id)
+      } else if (existingProduct) {
+        existingProduct.quantity -= 1
+      }
     }
-  },
+  }
 });
